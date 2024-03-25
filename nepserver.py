75a1ab962c56af3f12d32471c1090965f45e1e7d
WrenIX <dev@wrenix.eu>
2024-03-25 22:55:43 +0100

    fix(nepserver): use Datapoint from main and move enviroment variables

--- nepserver.py
+++ nepserver.py
@@ -3,7 +3,7 @@ import json
 import socketserver
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from datetime import datetime, timezone, timedelta
-from models import Datapoint
+from main import Datapoint
 from dnslib import DNSRecord, DNSHeader, RR, QTYPE, A
 from os import environ
 import threading
@@ -12,12 +12,24 @@ import socket
 # A dictionary to hold the last values received from POST requests.
 last_values = {}
 
+# Environment Variables for NEP-HTTP-Server Configuration
+LISTEN_ADDR = environ.get("NEP_LISTEN_ADDR", "localhost")
+LISTEN_PORT = int(environ.get("NEP_LISTEN_PORT", "8080"))
+
+MQTT_ADDR = environ.get("NEP_MQTT_ADDR", None)
+MQTT_PORT = int(environ.get("NEP_MQTT_PORT", "1883"))
+
+# Environment Variables for MQTT Home Assistant discovery
+MQTT_HA_EXPIRE = int(environ.get("NEP_MQTT_HA_EXPIRE", "3600"))
+MQTT_HA_MANUFACTURER = environ.get("NEP_▍MQTT_HA_MANUFACTURER", "NEP")
+MQTT_HA_MODEL = environ.get("NEP_MQTT_HA_MODEL", "BDM-800")
+
 # Environment Variables for DNS Configuration
 INTERCEPT_DOMAIN = 'www.nepviewer.net.'
 RESPONSE_IP = environ.get('RESPONSE_IP', '0.0.0.0')  # IP to respond with for intercepted domain
 FORWARD_DNS = environ.get('FORWARD_DNS', '8.8.8.8')  # DNS server to forward non-intercepted queries
 DNS_LISTEN_ADDR = environ.get("NEP_LISTEN_ADDR", "0.0.0.0")  # IP for DNS server to listen on
-DISABLE_DNS = environ.get('DISABLE_DNS', 'False').lower() in ['true', '1', 't']
+DISABLE_DNS = environ.get('DISABLE_DNS', 'False').lower() in ['true', '1', 't', 'yes']
 
 class DNSRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -29,7 +41,7 @@ class DNSRequestHandler(socketserver.BaseRequestHandler):
         if str(request.q.qname) == INTERCEPT_DOMAIN:
             # Intercept specific domain and respond with specified IP
             reply.add_answer(RR(request.q.qname, QTYPE.A, rdata=A(RESPONSE_IP), ttl=60))
-        elif FORWARD_DNS not in ["", "false", "False"]:
+        elif FORWARD_DNS.lower() not in ['', 'false', '0', 'f', 'no']:
             # Forward other DNS queries to a real DNS server
             try:
                 forward_reply = DNSRecord.parse(DNSRecord.question(request.q.qname).send(FORWARD_DNS, timeout=1))
@@ -42,7 +54,7 @@ class DNSRequestHandler(socketserver.BaseRequestHandler):
 
 class SimpleServer(BaseHTTPRequestHandler):
     def __init__(self, mqttc, *args):
-        self.mqttc = mqttc
+        self._mqttc = mqttc
         BaseHTTPRequestHandler.__init__(self, *args)
 
     def send_not_found(self):
@@ -52,10 +64,14 @@ class SimpleServer(BaseHTTPRequestHandler):
         self.wfile.write(b"NOT FOUND\n")
 
     def send_data_per_mqtt(self, dp: Datapoint, payload):
-        if self.mqttc is None:
+        if self._mqttc is None:
             return
+
+        self._mqttc.publish("nepserver/payload", payload.hex())
+        self._mqttc.publish(f"homeassistant/sensor/{dp.serial_number}/watt", dp.watt, True)
+
         serial_number_uppercase = dp.serial_number.upper()
-        self.mqttc.publish(f"homeassistant/sensor/{dp.serial_number}/watt/config", json.dumps({
+        self._mqttc.publish(f"homeassistant/sensor/{dp.serial_number}/watt/config", json.dumps({
             "name": f"Watt",
             "unique_id": f"mi_{dp.serial_number}_watt",
             "state_topic": f"homeassistant/sensor/{dp.serial_number}/watt",
@@ -63,7 +79,7 @@ class SimpleServer(BaseHTTPRequestHandler):
             "device_class": "power",
             "unit_of_measurement": "W",
             "state_class": "measurement",
-            "expire_after": 3600,
+            "expire_after": MQTT_HA_EXPIRE,
             "device": {
                 "identifiers": [
                     f"mi_{dp.serial_number}",
@@ -72,14 +88,13 @@ class SimpleServer(BaseHTTPRequestHandler):
                 ],
                 "name": f"MI-{serial_number_uppercase}",
                 "serial_number": serial_number_uppercase,
-                "manufacturer": "NEP",
-                "model": "BDM-600",
+                "manufacturer": MQTT_HA_MANUFACTURER,
+                "model": MQTT_HA_MODEL,
                 "sw_version": "unknown",
                 "hw_version": "unknown"
             }
         }), True)
-        self.mqttc.publish(f"homeassistant/sensor/{dp.serial_number}/watt", dp.watt, True)
-        self.mqttc.publish("nepserver/payload", payload.hex())
+
         print(f"send mqtt data: {dp}")
     
     def send_prometheus(self):
@@ -99,11 +114,17 @@ class SimpleServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
+
+        # json parser
         def output(obj):
+            # e.g. payload of datapoint
             if isinstance(obj, bytes):
                 return obj.hex()
+            # e.g. timestamp
             if isinstance(obj, datetime):
                 return obj.isoformat()
+            if isinstance(obj, Datapoint):
+                return dict((p, getattr(obj, p)) for p in dir(obj) if p[0] != "_" and isinstance(getattr(Datapoint, p), property))
             return obj.__dict__
         self.wfile.write(json.dumps(last_values, default=output).encode("utf-8"))
 
@@ -124,45 +145,51 @@ class SimpleServer(BaseHTTPRequestHandler):
 
         content_length = int(self.headers.get('Content-Length'))
         payload = self.rfile.read(content_length)
+
         dp = Datapoint().parse(payload)
+
+        # send datapoint and payload per mqtt 
         self.send_data_per_mqtt(dp, payload)
-        data = {
+
+        # store data in memory for prometheus metrics and data.json
+        last_values[dp.serial_number] = {
             "timestamp": datetime.now(),
             "datapoint": dp,
             "payload": payload,
         }
-        # store for prometheus metrics
-        last_values[dp.serial_number] = data
+
+        # dirty log (with payload)
         payload_str = payload.hex()
         print(f"Receive Data: {dp}, payload={payload_str}")
 
+        # send http response
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        # body contains current timestamp in UTC
         self.wfile.write(datetime.now(timezone(timedelta())).strftime("%Y%d%m%H%M%S").encode("utf-8"))
 
 def run_server():
-    listen_addr = environ.get("NEP_LISTEN_ADDR", "localhost")
-    listen_port = int(environ.get("NEP_LISTEN_PORT", "8080"))
 
-    mqtt_addr = environ.get("NEP_MQTT_ADDR", None)
-    mqtt_port = int(environ.get("NEP_MQTT_PORT", "1883"))
+    # setup mqtt
     mqttc = None
-    if mqtt_addr is not None:
+    if MQTT_ADDR is not None:
         try:
             import paho.mqtt.client as mqtt
             mqttc = mqtt.Client()
-            mqttc.connect(mqtt_addr, mqtt_port, 60)
+            mqttc.connect(MQTT_ADDR, MQTT_PORT, 60)
             mqttc.loop_start()
             print("connect to mqtt server")
         except:
             print("No mqtt support")
 
+    # bootstrap handler with extra arguments (e.g. mqtt)
     def handler(*args):
         SimpleServer(mqttc, *args)
-    web_server = HTTPServer((listen_addr, listen_port), handler)
-    print(f"Server started http://{listen_addr}:{listen_port}")
+    web_server = HTTPServer((LISTEN_ADDR, LISTEN_PORT), handler)
+    print(f"Server started http://{LISTEN_ADDR}:{LISTEN_PORT}")
 
+    # start everything
     try:
         web_server.serve_forever()
     except KeyboardInterrupt:
